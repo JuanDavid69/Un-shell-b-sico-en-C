95ed069a2e09a74a98672ff3c6f64c4664e08bbb
JuanDavid1998 <juan.gomez.orozco@correounivalle.edu.co>
2018-05-09 11:24:40 -0500

    Update MainShell.c

--- MainShell.c
+++ MainShell.c
@@ -1,6 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
 #include "obtener-palabras.h"
+
+int main(int argc, char const *argv[])
+{
+	while(1) {
+		char entrada[80];
+		char ** comando;
+		printf("#-> ");
+		scanf("%[^\n]%*c", entrada);
+		comando = obtenerPalabras(entrada);
+		if(!strcmp("exit", comando[0])) {
+			break;
+		}
+		
+
+		pid_t pid = fork();
+
+		if(!pid) {
+			execvp(comando[0], comando);
+			break;
+		} else {
+			wait(NULL);
+		}
+	}
+	
+	return 0;
+}
+
+/*
+int main(int argc, char const *argv[]){
+	while(1) {
+		char entrada[];
+		char ** comando;
+		printf("#-> ");
+		scanf("%s", entrada);
+		comando = obtenerPalabras(entrada)
+
+		if(!strcmp("exit", comando)) {
+			break;
+		}
+
+		pid_t pid = fork();
+
+		if(!pid) {
+			char * myargs[2];
+			myargs[0] = strdup(comando);
+			myargs[1] = NULL;
+			execvp(myargs[0], myargs);
+		} else {
+			wait(NULL);
+		}
+	}
+	return 0;
+}
+
 
  int main(int argc, char const *argv[]) {
    char entrada1[] = " a  -com --a";
@@ -11,4 +68,4 @@
      printf("%s\n", entradaProcesada1[i]);
    }
    return 0;
- }
+ }*/
