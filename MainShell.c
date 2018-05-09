9d0f8e4f619e6d0ceea2737bc31131cb8639fc50
JuanDavid1998 <juan.gomez.orozco@correounivalle.edu.co>
2018-05-09 17:05:30 -0500

    Main modularizado

--- MainShell.c
+++ MainShell.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <sys/wait.h>
 #include "obtener-palabras.h"
+#include "comandos.h"
 
 int main(int argc, char const *argv[])
 {
@@ -13,59 +14,14 @@ int main(int argc, char const *argv[])
 		printf("#-> ");
 		scanf("%[^\n]%*c", entrada);
 		comando = obtenerPalabras(entrada);
+
 		if(!strcmp("exit", comando[0])) {
 			break;
 		}
-		
-
-		pid_t pid = fork();
-
-		if(!pid) {
-			execvp(comando[0], comando);
+		if (comandoArgs(comando) == 1){
 			break;
-		} else {
-			wait(NULL);
 		}
 	}
 	
 	return 0;
 }
-
-/*
-int main(int argc, char const *argv[]){
-	while(1) {
-		char entrada[];
-		char ** comando;
-		printf("#-> ");
-		scanf("%s", entrada);
-		comando = obtenerPalabras(entrada)
-
-		if(!strcmp("exit", comando)) {
-			break;
-		}
-
-		pid_t pid = fork();
-
-		if(!pid) {
-			char * myargs[2];
-			myargs[0] = strdup(comando);
-			myargs[1] = NULL;
-			execvp(myargs[0], myargs);
-		} else {
-			wait(NULL);
-		}
-	}
-	return 0;
-}
-
-
- int main(int argc, char const *argv[]) {
-   char entrada1[] = " a  -com --a";
-   char ** entradaProcesada1;
-
-   entradaProcesada1 = obtenerPalabras(entrada1);
-   for (int i = 0; i < 3; i++) {
-     printf("%s\n", entradaProcesada1[i]);
-   }
-   return 0;
- }*/
