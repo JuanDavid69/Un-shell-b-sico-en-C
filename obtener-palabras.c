1c3b3cb31de5eb148bb67b7b9e82abb29b710aaf
JuanDavid1998 <juan.gomez.orozco@correounivalle.edu.co>
2018-05-08 21:24:56 -0500

    Update obtener-palabras.c

--- obtener-palabras.c
+++ obtener-palabras.c
@@ -64,15 +64,3 @@ char** obtenerPalabras(char entrada[]) {
     }
     return entradaProcesada;
 }
-//main para PRUEBA
-//
- int main(int argc, char const *argv[]) {
-   char entrada1[] = " a  -com --a";
-   char ** entradaProcesada1;
-
-   entradaProcesada1 = obtenerPalabras(entrada1);
-   for (int i = 0; i < 3; i++) {
-     printf("%s\n", entradaProcesada1[i]);
-   }
-   return 0;
- }
