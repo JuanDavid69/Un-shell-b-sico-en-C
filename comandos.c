7bfcd3dd86376bf33735ccbc7474e5a688612c11
JuanDavid1998 <juan.gomez.orozco@correounivalle.edu.co>
2018-05-09 19:49:29 -0500

    comandos

--- comandos.c
+++ comandos.c
@@ -4,11 +4,21 @@
 #include <string.h>
 #include <sys/wait.h>
 
-int comandoArgs (char ** comando){
+int comandoArgs (char ** comando, int size){
+	char ** comandoP;
+	comandoP = malloc(sizeof *comandoP * (size + 1));
+	for (int i = 0; i < size + 1; i++){
+		if ( i < size){
+			comandoP[i] = malloc(sizeof * comandoP[i] * (strlen(comando[i])));
+			comandoP[i] = strdup(comando[i]);
+		}else{
+			comandoP[i] = NULL;
+		}
+	}
 	int error = 0;
 	pid_t pid = fork();
 	if(!pid) {
-		execvp(comando[0], comando);
+		execvp(comandoP[0], comandoP);
 		printf("Error  al  ejecutar  el  comando \n");
 		error = 1;
 	} else {
