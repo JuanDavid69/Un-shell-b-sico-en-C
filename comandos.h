1458f3b22bbdde29609a170f05c099d69a9034d4
JuanDavid1998 <juan.gomez.orozco@correounivalle.edu.co>
2018-05-16 00:03:38 -0500

    Add files via upload

---
+++ comandos.h
@@ -0,0 +1,5 @@
+char ** agregarNull (char ** comando, int size);
+int comandoArgs (char ** comando, int size);
+int redireccionarSalida (char ** comando, char * nombre, int pos);
+int ejecutar(char ** comando, int size);
+
